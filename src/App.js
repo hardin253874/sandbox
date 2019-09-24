3522da04949e2e7779e17a2d199681f63e3dcf12
hardin253874 <hardin.253874@gmail.com>
2019-09-24 11:58:17 +1000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HelloMessage from './hello/HelloMessage';
+import Timer from './hello/Timer';
+import ToDoApp from './hello/ToDoApp';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"  />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+      </header>
+      <body>
+      <p>
+        <HelloMessage name="smart corp" />
+        And Show the Timer
+        <Timer />
+      </p>
+      <p>
+        To Do: <br />
+        <ToDoApp />
+      </p>
+
+      </body>
+    </div>
+  );
+}
+
+export default App;
